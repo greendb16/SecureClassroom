8c8828aa98fc4b8a83306db2e35319f128cf8931
greendb16 <greendb16@gmail.com>
2019-07-24 16:09:27 -0400

    end of 4.05

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
@@ -10,5 +13,17 @@ public class HomeController {
     @RequestMapping("/")
     public String index(){
         return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @RequestMapping("/secure")
+    public String  secure(Principal principal, Model model){
+        String username = principal.getName();
+        model.addAttribute("user", repository.findByUsername(username));
+        return "secure";
     }
 }
--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -1,13 +1,15 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -18,20 +20,43 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
         return new BCryptPasswordEncoder();
     }
 
+    @Autowired
+    private SSUserDetailsService userDetailsService;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public UserDetailsService userDetailsServiceBean() throws Exception{
+        return new SSUserDetailsService(userRepository); }
+
+    @Override
+    protected void configure(HttpSecurity http)
+            throws Exception {
         http
-                .authorizeRequests()
+                .authorizeRequests().antMatchers("/", "/h2-console/**").permitAll()
+                .antMatchers("/admin")
+                .access("hasAuthority('ADMIN')")
                 .anyRequest().authenticated()
                 .and()
-                .formLogin();
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout().logoutRequestMatcher(
+                new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login").permitAll()
+                .and()
+                .httpBasic();
+        http
+                .csrf().disable();
+        http
+                .headers().frameOptions().disable();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth)
-        throws Exception{
-            auth.inMemoryAuthentication().withUser("user")
-            .password(passwordEncoder().encode("password"))
-            .authorities("user");
+        throws Exception
+    {
+        auth.userDetailsService(userDetailsService())
+            .passwordEncoder(passwordEncoder());
     }
 }
